6a03800841e7d5bdd17b3f112faeec76ef6d2eb6
Madeline <mmcintosh2k14@gmail.com>
2019-10-04 10:55:39 -0400

    adds completed header section

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const container = document.querySelector('.header-container');
+console.log(container);
+
+function header() {
+    //create each element
+    const headerContainer = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // append each element where it belongs
+    headerContainer.appendChild(date);
+    headerContainer.appendChild(title);
+    headerContainer.appendChild(temp);
+
+    // add class to each element
+    headerContainer.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // add text content to each element
+    date.textContent = "MARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    return headerContainer
+}
+
+container.append(header());
